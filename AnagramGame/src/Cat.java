12cc90110643a0020206a4fb05d0ae1e2154f400
Anna <anghiloglu@gmail.com>
2019-03-15 20:42:33 +0200

    kjhgfdsdfghjk

---
+++ AnagramGame/src/Cat.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author User
+ */
+public class Cat {
+    public static void main (String[] args)
+    {System.out.println("Meau");
+    System.out.println("Welcom to the main methode of my application!");
+    }
+           
+   }
---
+++ AnagramGame/src/class/Cat.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author User
+ */
+public class Cat {
+    public static void main (String[] args)
+    {System.out.println("Meau");
+    System.out.println("Welcom to the main methode of my application!");
+    System.out.println("Param 1:" + args[0]);
+    System.out.println("Param 2:" + args[1]);
+    }
+           
+   }
+
